39b726b9dc8b34c64777854723cd735174ab9987
Isaac-opz <piedrahitaisaac7@gmail.com>
2023-10-28 15:34:21 -0500

    create-property-function-fixed

--- microproperties/src/models/property.js
+++ microproperties/src/models/property.js
@@ -15,61 +15,34 @@ const getProperties = async (filters) => {
 };
 
 // Función para crear propiedades
-const createProperty = async (req, res) => {
-    try {
-        const {
-            nombre,
-            id_anfitrion,
-            nombre_anfitrion,
-            distrito,
-            barrio,
-            latitud,
-            longitud,
-            tipo_habitacion,
-            precio,
-            minimo_noches,
-            numero_opiniones,
-            ultima_opinion,
-            opiniones_por_mes,
-            propiedades_del_anfitrion,
-            dias_habiles,
-            calificacion
-        } = req.body;
+const createProperty = async (propertyData) => {
+    const query = `
+        INSERT INTO libertyhub.propiedades
+        (nombre, id_anfitrion, nombre_anfitrion, distrito, barrio, latitud, longitud, tipo_habitacion, precio, minimo_noches, numero_opiniones, ultima_opinion, opiniones_por_mes, propiedades_del_anfitrion, dias_habiles, calificacion)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+    `;
 
-        // Validación básica de los campos
-        if (!nombre || !id_anfitrion || !nombre_anfitrion || !distrito || !barrio || !latitud || !longitud || !tipo_habitacion || precio == null || minimo_noches == null || numero_opiniones == null || !ultima_opinion || opiniones_por_mes == null || propiedades_del_anfitrion == null || dias_habiles == null || calificacion == null) {
-            return res.status(400).json({ message: 'Todos los campos son requeridos' });
-        }
+    const values = [
+        propertyData.nombre,
+        propertyData.id_anfitrion,
+        propertyData.nombre_anfitrion,
+        propertyData.distrito,
+        propertyData.barrio,
+        propertyData.latitud,
+        propertyData.longitud,
+        propertyData.tipo_habitacion,
+        propertyData.precio,
+        propertyData.minimo_noches,
+        propertyData.numero_opiniones,
+        propertyData.ultima_opinion,
+        propertyData.opiniones_por_mes,
+        propertyData.propiedades_del_anfitrion,
+        propertyData.dias_habiles,
+        propertyData.calificacion
+    ];
 
-        const propertyData = {
-            nombre,
-            id_anfitrion,
-            nombre_anfitrion,
-            distrito,
-            barrio,
-            latitud,
-            longitud,
-            tipo_habitacion,
-            precio,
-            minimo_noches,
-            numero_opiniones,
-            ultima_opinion,
-            opiniones_por_mes,
-            propiedades_del_anfitrion,
-            dias_habiles,
-            calificacion
-        };
-
-        const result = await Property.createProperty(propertyData);
-
-        if (result.affectedRows > 0) {
-            res.status(201).json({ message: 'Propiedad creada con éxito', id: result.insertId });
-        } else {
-            res.status(400).json({ message: 'Error al crear la propiedad' });
-        }
-    } catch (error) {
-        res.status(500).json({ message: 'Error al crear la propiedad' });
-    }
+    const [result] = await connection.execute(query, values);
+    return result;
 };
 
 const getPropertyById = async (id) => {
