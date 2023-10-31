6b8554ac2100d4edd931174c101646f82d9689fe
d-salamanca <d.salamanca0314@gmail.com>
2023-10-31 11:10:37 -0500

    cambios microresena

--- microresena/src/controllers/resenaController.js
+++ microresena/src/controllers/resenaController.js
@@ -1,25 +1,31 @@
+const axios = require('axios');
 const resenaModel = require('../models/resenaModel');
 
 exports.obtenerResenas = async (req, res) => {
     const propiedadId = req.params.propiedadId;
     try {
-        const reseñas = await resenaModel.obtenerReseñasDePropiedad(propiedadId);
-        res.json(reseñas);
+        // Obteniendo las propiedades mediante una petición axios al propertiesController
+        const response = await axios.get(`http://localhost:{YOUR_PROPERTIES_PORT}/propiedades/${propiedadId}`);
+        
+        const propiedades = response.data;
+        const resenas = await resenaModel.obtenerResenasDePropiedad(propiedadId);
+        
+        res.json({ propiedades, resenas });
     } catch (error) {
-        res.status(500).json({ message: 'Error al obtener las reseñas de la propiedad' });
+        res.status(500).json({ message: 'Error al obtener las resenas de la propiedad' });
     }
-}
+};
 
 exports.crearResena = async (req, res) => {
-    const {comentarios, ranking} = req.body;
+    const { comentarios, ranking } = req.body;
     try {
-        const nuevaReseña = {
+        const nuevaResena = {
             comentarios,
             ranking
         };
-        await resenaModel.crearReseña(nuevaReseña);
-        res.json({ message: 'Reseña creada exitosamente' });
+        await resenaModel.crearResena(nuevaResena);
+        res.json({ message: 'Resena creada exitosamente' });
     } catch (error) {
-        res.status(500).json({ message: 'Error al crear la reseña' });
+        res.status(500).json({ message: 'Error al crear la resena' });
     }
-}
+};
