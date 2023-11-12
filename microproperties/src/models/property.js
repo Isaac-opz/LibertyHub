8a5bcbee5cce2ce3e41d5a5938fbc1a98f3d9d5b
Isaac-opz <piedrahitaisaac7@gmail.com>
2023-11-12 11:20:04 -0500

    verify-user-on-review&minor-details

--- microproperties/src/models/property.js
+++ microproperties/src/models/property.js
@@ -1,5 +1,4 @@
 const mysql = require('mysql2/promise');
-const reviewModel = require('../../../microresena/src/models/reviewModel');
 const connection = mysql.createPool({
     host: 'localhost',
     user: 'root',
